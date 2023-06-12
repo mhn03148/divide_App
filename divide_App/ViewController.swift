4f4514b92e413f9a5933e6550596c7ac764876b2
jujinhyeong <mhn03148@naver.com>
2023-06-12 20:45:37 +0900

    Initial Commit

---
+++ divide_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  divide_App
+//
+//  Created by 주진형 on 2023/06/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
