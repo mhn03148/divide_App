7bf55da473f4e52111a3aa3e76e80d11dd7f4e22
jujinhyeong <mhn03148@naver.com>
2023-06-12 21:30:00 +0900

    6/12 실습 복습 나눗셈 어플 0일때 guard else 써서 예외구문 학습

--- divide_App/ViewController.swift
+++ divide_App/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var firstNumText: UITextField!
+    
+    @IBOutlet var secondNumText: UITextField!
+    
+    @IBOutlet var resultText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func divideButton(_ sender: Any) {
+        let firstNumber: Double = Double(firstNumText.text ?? "") ?? 0.0
+        let secondNumber: Double = Double(secondNumText.text ?? "") ?? 0.0
+        guard secondNumber != 0 else {
+            resultText.text = "Can't divide it"
+            return
+        }
+        let resultNumber: Double = firstNumber / secondNumber
+        //resultNumber = firstNumber / secondNumber
+        resultText.text = "\(resultNumber)"
+    }
+    
 }
 
